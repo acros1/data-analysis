93095cc52a5e247687d5bf832d8528c55dd39c44
Alexandre Cros <alexandre.cros58@gmail.com>
2020-12-11 16:29:53 +0100

    commit csv, and working path finding into info.r

--- src/info.R
+++ src/info.R
@@ -3,16 +3,19 @@
 library("ggplot2")
 library("dplyr")
 
-#the dataset, called "database", is imported through the "import dataset" feature
-#there is not point in using the read() function as the dataset needs to be extracted from an archive so the
-#path would need to be changed on every machine
+# Path to dataset is creating by this trick
+scriptPath = paste(dirname(rstudioapi::getSourceEditorContext()$path), "/../dataset")
+# And this trick to delete a parasite space
+scriptPath = gsub ("src ", "src", scriptPath)
+# homicides
+homicides = read.csv(file = file.path(scriptPath, "homicide-reports-1980_2014.csv"), sep = ",")
 
-rowNb = nrow(database)
-columnNb = ncol(database)
+rowNb = nrow(homicides)
+columnNb = ncol(homicides)
 sprintf("There are %i columns and %i rows in this database",columnNb,rowNb)
 
 #first, we look at the number of homicides per year
-gp_perYear <- database %>% group_by(Year)
+gp_perYear <- homicides %>% group_by(Year)
 #gp_perYear <- gp_perYear %>% group_by(Record.ID)
 plotPerYear <- gp_perYear %>% summarise(nb = n())
 
@@ -24,7 +27,7 @@ ggplot() +
 
 #what the fuck happened in the 90s
 
-plotPerState <- database %>% group_by(Year, State) %>% summarise(nb = n())
+plotPerState <- homicides %>% group_by(Year, State) %>% summarise(nb = n())
 ggplot(data = plotPerState,aes(x = Year, y = nb), color=State) +
   geom_line() +
   facet_wrap("State")
